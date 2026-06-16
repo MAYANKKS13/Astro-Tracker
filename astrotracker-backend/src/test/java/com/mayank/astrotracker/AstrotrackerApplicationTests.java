9946586d904f1a596f3d094072d1eb9309166e93
mayankksinha1 <mayankksinha1@gmail.com>
2026-06-17 01:04:59 +0530

    Updated folder structure backend.

---
+++ astrotracker-backend/src/test/java/com/mayank/astrotracker/AstrotrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.astrotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrotrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
